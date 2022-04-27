d50062242fd4fccbe04d5b020f536f6eddf1c303
Shreayaa <shreyaa2095@gmail.com>
2022-04-27 11:19:38 -0400

    two

--- fooodddd/src/components/BusinessList/BusinessList.js
+++ fooodddd/src/components/BusinessList/BusinessList.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
-import './BusinessList.css'
-import Business from './Business/Business'
+import '../BusinessList/BusinessList.css'
+import Business from '../Business/Business.js'
 
 
 class BusinessList extends React.Component { 
--- fooodddd/src/components/SearchBar/SearchBar.js
+++ fooodddd/src/components/SearchBar/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import '../SearchBar';
+import '../SearchBar/SearchBar.css';
 
 let sortByOptions = { 
   'Best Match': "best_match",
@@ -9,7 +9,31 @@ let sortByOptions = {
 
 class SearchBar extends React.Component { 
   renderSortByOptions() { 
-    return(Object.keys(sortByOptions)).map(sortByOptions);
+    Object.keys(sortByOptions).map(sortByOption => { 
+      let sortByOptionValue = sortByOptions(sortByOption);
+      return <li key={sortByOptionValue}>{sortByOption}</li>
+    });
+  };
+
+  render() { 
+    return ( 
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+           thi.renderSortByOptions()
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
+      </div>
+    )
   }
 
 }
+
+export default SearchBar;
